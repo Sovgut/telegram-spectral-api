1274c4a454f82c339ceee5b8c7f6f5e36ad6f6ab
Sergey Sovgut <sergey.sovgut@outlook.com>
2023-04-27 11:11:28 +0300

    Refactored publish/text endpoint and related services/providers.

--- source/services/publish/text.ts
+++
@@ -1,7 +0,0 @@
-import { App } from "../../main.js";
-
-export async function sendTextMessage(chatId: string, text: string) {
-    await App.bot.telegram.sendMessage(chatId, text, {
-        parse_mode: 'HTML'
-    });
-}
